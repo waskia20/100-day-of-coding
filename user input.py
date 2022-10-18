72ed22d240e859f3d9534b16c54eea87b06f15e8
waskia20 <waskiatinambung@gmail.com>
2022-10-18 16:23:37 +0700

    test

---
+++ user input.py
@@ -0,0 +1,12 @@
+Nama_user = input("Masukkan Nama Kamu:")
+print(Nama_user)
+
+Umur = int(input("Masukkan Umur Kamu:"))
+print(Umur)
+print(type(Umur))
+
+suhu_badan = float (input("masukkan suhu badan:"))
+print(suhu_badan)
+print(type(suhu_badan))
+
+
