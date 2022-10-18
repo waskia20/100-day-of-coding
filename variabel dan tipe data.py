a7abd97b51796672b35c42d5ac22a94543dd3be9
waskia20 <waskiatinambung@gmail.com>
2022-10-18 18:12:08 +0700

    day1

---
+++ variabel dan tipe data.py
@@ -0,0 +1,9 @@
+Nama = "waskia"
+Umur = 20
+Makanan_favorite = "pangsit_bakso"
+Alamat =" dusun_lambepada"
+
+print("Nama = ",Nama)
+print("umur = ",Umur)
+print("Makanan_favorite = ",Makanan_favorite)
+print("Alamat = ",Alamat)
