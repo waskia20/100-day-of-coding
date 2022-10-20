e29583f7972dae6fbd1d7acecd982540bb16e375
waskia20 <waskiatinambung@gmail.com>
2022-10-20 21:56:01 +0700

    DAY 4

---
+++ operasi aritmatika.py
@@ -0,0 +1,32 @@
+ #operasi aritmatika
+
+a = 8
+b = 3
+#penjumlahan +
+hasil = a + b
+print(a,"+", b,"=",hasil)
+#pengurangan -
+hasil = a - b
+print(a,"-",b,"=",hasil)
+#perkalian *
+hasil = a * b
+print(a,"x", b,"=",hasil)
+#pembagian /
+hasil = a / b
+print(a,"/",b,"=",hasil)
+
+#pangkat **
+hasil = a ** b
+print(a,"pangkat",b,"=",hasil)
+#modulus % (sisa pembagian)
+hasil = a % b
+print(a,"%",b,"=",hasil)
+#floar division // (kebalikan dari modulus
+hasil = a // b
+print(a,"//",b,"=",hasil)
+#prioritas operasi
+x = 2
+y = 3
+z = 4
+hasil2 = (x +y)*(z - y)/(y+z)
+print("hasil2 = ",hasil2)
