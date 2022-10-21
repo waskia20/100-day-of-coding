0f724ef8e66710f11f9621a8bbc5d62e097912d4
waskia20 <waskiatinambung@gmail.com>
2022-10-21 10:57:39 +0700

    DAY5

---
+++ operator_penugasan.py
@@ -0,0 +1,27 @@
+#operator penugasan
+x = 5
+print(x)
+
+x += 2
+print(x)
+
+x -= 2
+print(x)
+
+x *= 2
+print(x)
+
+x /= 2
+print(x)
+
+x %= 2
+print(x)
+
+x //= 2
+print(x)
+
+x **= 2
+print(x)
+
+
+
