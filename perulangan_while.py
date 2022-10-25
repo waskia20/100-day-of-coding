d323033b907f3fbe03b99c0ebf44a67b17894561
waskia20 <waskiatinambung@gmail.com>
2022-10-25 22:30:53 +0700

    pesan

---
+++ perulangan_while.py
@@ -0,0 +1,31 @@
+#perulagan while
+#1
+i = 1
+while i <= 3:
+    print('dasar pemprograman')
+    i += 1
+    #loop
+
+#2
+i = 1
+while i <= 5:
+    print('dasar pemprograman')
+    #infinity loop
+
+#3
+i = 2
+while i <= 100:
+    print(i)
+    i = i+2
+#4
+i = 5
+    print('dasar pemprograman', i)
+i -= 1
+
+#5
+i = 1
+while i <= 3:
+    print('dasar pemrograman',i)
+    i += 1
+else:
+    print("perulangan sudah lebih dari 3")
