88c3a26124858b3f4be0164f81b5e8493788265d
waskia20 <waskiatinambung@gmail.com>
2022-10-29 21:14:18 +0700

    DAY_13

---
+++ pengenalan_ fungsi.py
@@ -0,0 +1,11 @@
+#Membuat fungsi
+
+def hello_world():
+    '''fungsi menampilkan hello world'''
+    print("hello world")
+    print("hello saya waskia")
+    print("salam kenal ")
+
+hello_world()
+hello_world()
+hello_world()
