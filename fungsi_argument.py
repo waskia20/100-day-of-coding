d23f891fc02b44794763717749e6bda60e19525c
waskia20 <waskiatinambung@gmail.com>
2022-10-30 22:52:11 +0700

    DAY_14

---
+++ fungsi_argument.py
@@ -0,0 +1,23 @@
+'''fungsi dengan argument'''
+
+#Template
+#def nama_fungsi(argument):
+#badan fungsi
+
+def hello_world(nama):
+    '''fungsi hello world menerima input dengan variable nama'''
+    print(f"semangat untuk 100 day of coding  {nama}")
+
+hello_world("waskia")
+hello_world("hawa")
+hello_world("arfan")
+
+#program tambah
+def tambah(angka_1,angka_2):
+    '''fungsi tambah'''
+    hasil=angka_1 +angka_2
+    print(f"{angka_1}+{angka_2}={hasil}")
+
+tambah(1,5)
+tambah(100000,1)
+
